ce8a18c176695b4f6261eeecc38d1fc8c8113065
Ramprits <rampritsahani@outlook.in>
2019-07-09 15:57:49 +0530

    1 file modified

--- src/app/fruit/fruit-list/fruit-list.component.ts
+++ src/app/fruit/fruit-list/fruit-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FruitService } from '../fruit.service';
 @Component({
   selector: 'app-fruit-list',
   templateUrl: './fruit-list.component.html',
   styleUrls: ['./fruit-list.component.css']
 })
 export class FruitListComponent implements OnInit {
-
-  constructor() { }
+  fruits: any[];
+  constructor(private fs: FruitService) { }
 
   ngOnInit() {
+    this.fs.getFruits().subscribe(fruit => console.log(this.fruits = fruit))
   }
 
 }
